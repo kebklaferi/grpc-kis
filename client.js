0edef5164c16acf0ee0ebee518b86f08ad28f444
klarakeb <klara.kebric@student.um.si>
2023-12-14 15:33:19 +0100

    end commit

--- client.js
+++ client.js
@@ -16,16 +16,100 @@ const run = async () => {
         const productService = protoDescriptor.ProductService;
         const client = new productService('0.0.0.0:50051', grpc.credentials.createInsecure());
         const tmp = {
-            alias: "testni-produkt-2",
-            title: "Testni produkt 2",
-            brand: 4,
+            id: 8,
+            alias: "updated-produkt",
+            title: "updated produkt",
+            brand: 3,
             category: 4,
         }
-        await createProduct(client, tmp)
+        const ing = {
+            id: 6,
+            alias: "update-sestavina-4",
+            title: "Update sestavina 4",
+            description: "opis",
+            acne_fighting: 0,
+            comedogenic_rating: 1
+        }
+        await getIngredients(client)
 
     } catch (error) {
         console.error(error)
     }
+}
+function deleteIngredient(client, id){
+    client.DeleteIngredient({id: id}, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    });
+}
+function getIngredients(client){
+    const stream = client.GetIngredients();
+    stream.on("data", (chunk) => {
+        console.log(chunk)
+    })
+    stream.on("end", () => {
+        console.log("Stream data received.")
+    })
+}
+function updateIngredient(client, ingredient){
+    client.UpdateIngredient(ingredient, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    });
+}
+function createIngredient(client, ingredient){
+    client.CreateIngredient(ingredient, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    });
+}
+function getIngredient(client, id){
+    client.GetIngredient({id: id}, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    })
+}
+
+function getProductCategory(client, id) {
+    client.GetProductCategory({id: id}, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    })
+}
+
+function getProductBrand(client, id) {
+    client.GetProductBrand({id: id}, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    })
+}
+
+function updateProduct(client, product) {
+    client.UpdateProduct(product, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(result);
+    })
 }
 
 function deleteProduct(client, id) {
@@ -37,7 +121,6 @@ function deleteProduct(client, id) {
         console.log(result);
     });
 }
-
 
 async function getProduct(client, id) {
     try {
--- service-impl.js
+++ service-impl.js
@@ -20,7 +20,6 @@ const getProduct = async (req, res) => {
             details: "Internal Server Error"
         });
     }
-
 }
 const createProduct = async (req, res) => {
     const {alias, title, brand, category} = req.request;
@@ -55,7 +54,7 @@ const deleteProduct = async (req, res) => {
         } else {
             const delRows = await knex("product").where({id: id}).del();
             res(null, {
-                message: delRows + "row(s) affected.",
+                message: delRows + " row(s) affected.",
                 status: grpc.status.OK
             })
         }
@@ -68,61 +67,231 @@ const deleteProduct = async (req, res) => {
     }
 }
 const updateProduct = async (req, res) => {
-
+    const {alias, title, brand, category, id} = req.request;
+    try {
+        const exists = await knex("product").where({id: id}).first();
+        if (!exists) {
+            res({
+                code: grpc.status.NOT_FOUND,
+                details: "Resource does not exist."
+            })
+        } else {
+            const result = await knex("product").where({id: id})
+                .update({
+                    alias: alias,
+                    title: title,
+                    brand_id: brand,
+                    category_id: category,
+                })
+            res(null, {
+                message: result + " row(s) affected.",
+                status: grpc.status.OK
+            })
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
+    }
 }
-    const getProducts = (call) => {
-        knex.select("*")
-            .from("product")
-            .then((products) => {
-                for (let product of products) {
-                    console.log(product)
-                    call.write(product)
-                }
-                call.end();
-                console.log("Done streaming data.")
+const getProducts = (call) => {
+    knex.select("*")
+        .from("product")
+        .then((products) => {
+            for (let product of products) {
+                console.log(product)
+                call.write(product)
+            }
+            call.end();
+            console.log("Done streaming data.")
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+}
+
+/*  INGREDIENTS  */
+const getIngredient = async (req, res) => {
+    const {id} = req.request;
+    try {
+        const ingredient = await knex("ingredient").where({id: id}).first();
+        res(null, {
+            id: ingredient.id,
+            title: ingredient.title,
+            alias: ingredient.alias,
+            description: ingredient.description,
+            acne_fighting: ingredient.acne_fighting,
+            comedogenic_rating: ingredient.comedogenic_rating
+        })
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
+    }
+}
+const createIngredient = async (req, res) => {
+    const {alias, title, description, acne_fighting, comedogenic_rating} = req.request;
+    try {
+        const id = await knex("ingredient").insert({
+            alias: alias,
+            title: title,
+            description: description,
+            acne_fighting: acne_fighting,
+            comedogenic_rating: comedogenic_rating,
+        })
+        res(null, {
+            message: "Successfully created ingredient with id " + id,
+            status: grpc.status.OK
+        })
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
+    }
+}
+const deleteIngredient = async (req, res) => {
+    const {id} = req.request
+    try {
+        const exists = await knex("ingredient").where({id: id}).first();
+        if (!exists) {
+            res({
+                code: grpc.status.NOT_FOUND,
+                details: "Resource does not exist."
             })
-            .catch(error => {
-                console.error("Error:", error);
+        } else {
+            const delRows = await knex("ingredient").where({id: id}).del();
+            res(null, {
+                message: delRows + " row(s) affected.",
+                status: grpc.status.OK
             })
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
     }
-
-    /*  INGREDIENTS  */
-    const getIngredient = (req, res) => {
-
+}
+const updateIngredient = async (req, res) => {
+    const {alias, title, description, acne_fighting, comedogenic_rating, id} = req.request;
+    try {
+        const exists = await knex("ingredient").where({id: id}).first();
+        if (!exists) {
+            res({
+                code: grpc.status.NOT_FOUND,
+                details: "Resource does not exist."
+            })
+        } else {
+            const result = await knex("ingredient").where({id: id})
+                .update({
+                    alias: alias,
+                    title: title,
+                    description: description,
+                    acne_fighting: acne_fighting,
+                    comedogenic_rating: comedogenic_rating,
+                })
+            res(null, {
+                message: result + " row(s) affected.",
+                status: grpc.status.OK
+            })
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
     }
-    const createIngredient = (req, res) => {
+}
+const getIngredients = (call) => {
+    knex.select("*")
+        .from("ingredient")
+        .then((ingredients) => {
+            for (let ingredient of ingredients) {
+                console.log(ingredient)
+                call.write(ingredient)
+            }
+            call.end();
+            console.log("Done streaming data.")
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+}
 
+const getProductCategory = async (req, res) => {
+    const {id} = req.request
+    try {
+        const exists = await knex("product").where({id: id}).first();
+        if (!exists) {
+            res({
+                code: grpc.status.NOT_FOUND,
+                details: "Resource does not exist."
+            })
+        } else {
+            const categoryId = await knex("product").select("category_id").where({id: id}).first();
+            const categoryDetails = await knex("category").where({id: categoryId.category_id}).first();
+            res(null, {
+                id: categoryDetails.id,
+                alias: categoryDetails.alias,
+                title: categoryDetails.title
+            })
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
     }
-    const deleteIngredient = (req, res) => {
+}
 
+const getProductBrand = async (req, res) => {
+    const {id} = req.request
+    try {
+        const exists = await knex("product").where({id: id}).first();
+        if (!exists) {
+            res({
+                code: grpc.status.NOT_FOUND,
+                details: "Resource does not exist."
+            })
+        } else {
+            const brandId = await knex("product").select("brand_id").where({id: id}).first();
+            const brandDetails = await knex("brand").where({id: brandId.brand_id}).first();
+            res(null, {
+                id: brandDetails.id,
+                alias: brandDetails.alias,
+                title: brandDetails.title
+            })
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res({
+            code: grpc.status.INTERNAL,
+            details: "Internal Server Error"
+        });
     }
-    const updateIngredient = (req, res) => {
-
-    }
-    const getIngredients = (call) => {
-
-    }
-
-    const getProductCategory = () => {
-
-    }
-
-    const getProductBrand = () => {
-
-    }
+}
 
 
-    module.exports = {
-        getProduct,
-        deleteProduct,
-        updateProduct,
-        createProduct,
-        getProducts,
-        getIngredient,
-        deleteIngredient,
-        updateIngredient,
-        createIngredient,
-        getIngredients,
-        getProductBrand,
-        getProductCategory
-    }
+module.exports = {
+    getProduct,
+    deleteProduct,
+    updateProduct,
+    createProduct,
+    getProducts,
+    getIngredient,
+    deleteIngredient,
+    updateIngredient,
+    createIngredient,
+    getIngredients,
+    getProductBrand,
+    getProductCategory
+}
